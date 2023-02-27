2fb58f4e6f4195a7428db3ed26324f7a60ae985d
aldistate <aldistate@yahoo.com>
2023-02-27 12:50:44 +0700

    22. Update Admin Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +24,23 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // DB::table('users')->insert([
+        //     [
+        //         // 'id' => 3,
+        //         'name' => 'admin',
+        //         'email' => 'admin@admin.com',
+        //         'password' => Hash::make('password'),
+        //         'is_admin' => true,
+        //     ]
+        // ]);
+
+        User::create([
+            // 'id' => 3,
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'is_admin' => true,
+        ]);
     }
 }
